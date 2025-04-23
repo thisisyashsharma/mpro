df31e1bb4fdcb511b3c229d9b9e874698a798823
Yash Sharma <91525996+thisisyashsharma@users.noreply.github.com>
2025-04-23 16:06:11 +0530

    Add files via upload

---
+++ floatBtn/Bar.js
@@ -0,0 +1,34 @@
+<div class="container">
+  <li>
+    <div>
+      <a href="/quotes" class="rLink">
+        A
+      </a>
+    </div>
+    <span>Home</span>
+  </li>
+  <li>
+    <div>
+      <a href="/new-quote" class="rLink">
+        B
+      </a>
+    </div>
+    <span>Create</span>
+  </li>
+  <li>
+    <div>
+      <a href="/about" class="rLink">
+        C
+      </a>
+    </div>
+    <span>About</span>
+  </li>
+  <li>
+    <div>
+      <a class="rLink" href="/Contact">
+        D
+      </a>
+    </div>
+    <span>Contact</span>
+  </li>
+</div>;
